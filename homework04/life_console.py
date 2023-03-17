82106c0960f16ae14fc42936f72b561545e123bc
lexozbedra <viktoria@rudnikov.com.ru>
2023-03-17 19:49:55 +0300

    game of life

---
+++ homework04/life_console.py
@@ -0,0 +1,46 @@
+import curses
+import time
+
+from life import GameOfLife
+from ui import UI
+
+
+def draw_borders(screen) -> None:
+    """Отобразить рамку."""
+    screen.border("|", "|", "-", "-", "+", "+", "+", "+")
+
+
+class Console(UI):
+    def __init__(self, life: GameOfLife) -> None:
+        super().__init__(life)
+        self.game = life
+
+    def draw_borders(self, screen) -> None:
+        """Отобразить рамку."""
+        screen.border("|", "|", "-", "-", "+", "+", "+", "+")
+
+    def draw_grid(self, screen) -> None:
+        """Отобразить состояние клеток."""
+        grid = self.game.curr_generation
+        for i in range(len(grid)):
+            screen.addstr(i + 1, 1, "".join(map(str, grid[i])).replace("1", "*").replace("0", " "))
+
+    def run(self) -> None:
+        screen = curses.initscr()  # type: ignore
+        screen.nodelay(True)
+        screen.resize(self.game.rows + 2, self.game.cols + 2)
+
+        self.draw_borders(screen)
+        draw_borders(screen)
+
+        while screen.getch() != ord("q"):
+            self.draw_grid(screen)
+            self.life.step()
+            screen.refresh()
+            time.sleep(0.1)
+        curses.endwin()  # type: ignore
+
+
+game = GameOfLife((24, 32))
+ui = Console(game)
+ui.run()
---
+++ homework04/life_gui.py
@@ -0,0 +1,63 @@
+import pygame
+from pygame.locals import *
+
+from life import GameOfLife
+from ui import UI
+
+
+class GUI(UI):
+    def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
+        super().__init__(life)
+        self.cell_size = cell_size
+        self.speed = speed
+        self.rows, self.cols = life.rows, life.cols
+        self.screen_size = self.cols * self.cell_size, self.rows * self.cell_size
+        self.screen = pygame.display.set_mode(self.screen_size)
+
+    def draw_lines(self) -> None:
+        for x in range(0, self.cols * self.cell_size, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.rows * self.cell_size))
+        for y in range(0, self.rows * self.cell_size, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.cols * self.cell_size, y))
+
+    def draw_grid(self) -> None:
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.life.curr_generation[i][j] == 1:
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color("green"),
+                        (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size),
+                    )
+                else:
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color("white"),
+                        (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size),
+                    )
+        self.draw_lines()
+
+    def run(self) -> None:
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
+        self.draw_lines()
+        self.draw_grid()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+            self.draw_lines()
+            self.draw_grid()
+            self.life.step()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    game = GameOfLife((50, 50), randomize=True)
+    gui = GUI(game, 10, 10)
+    gui.run()
