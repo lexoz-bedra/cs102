7add7ecea3d1175986a3349b7107a3c0ad419d88
lexozbedra <viktoria@rudnikov.com.ru>
2023-05-16 12:48:42 +0300

    isort

--- homework07/hackernews.py
+++ homework07/hackernews.py
@@ -1,6 +1,5 @@
-from bottle import redirect, request, route, run, template  # type: ignore
-
 from bayes import NaiveBayesClassifier
+from bottle import redirect, request, route, run, template  # type: ignore
 from db import News, session
 from scraputils import get_news
 
